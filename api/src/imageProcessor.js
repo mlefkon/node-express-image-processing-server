482405dfea669a933139e3112b2b5a8b2e2553aa
Marc Lefkon <marc@leftek.com>
2023-03-24 14:41:01 +0800

    a

---
+++ api/src/imageProcessor.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const { Worker, isMainThread } = require('worker_threads');
+
+const pathToResizeWorker = path.resolve(__dirname, 'resizeWorker.js');
+const pathToMonochromeWorker = path.resolve(__dirname, 'monochromeWorker.js');
+
+function imageProcessor(filename) {
+    const sourcePath = uploadPathResolver(filename);
+    const resizedDestination = uploadPathResolver('resized-' + filename);
+    const monochromeDestination = uploadPathResolver('monochrome-' + filename);
+    const resizeWorkerFinished = false;
+    const monochromeWorkerFinished = false;
+    return new Promise( (resolve, reject) => {
+        if (isMainThread()) {
+            try {
+                const resizeWorker     = new Worker(pathToResizeWorker,     { workerData: { source: sourcePath, sourcePath: resizedDestination    } })
+                const monochromeWorker = new Worker(pathToMonochromeWorker, { workerData: { source: sourcePath, sourcePath: monochromeDestination } })
+                resizeWorker.on('message', (message) => {
+                    resizeWorkerFinished = true;
+                    if (monochromeWorkerFinished === true) {
+                        resolve('resizeWorker finished processing');
+                    }
+                })
+                resizeWorker.on('error', (error) => {
+                    reject(new Error(error.message));
+                })
+                resizeWorker.on('exit', (code) => {
+                    if (code !== 0) {
+                        reject(new Error('Exited with status code ' + code));
+                    }
+                })
+                monochromeWorker.on('message', (message) => {
+                    monochromeWorkerFinished = true;
+                    if (resizeWorkerFinished === true) {
+                        resolve('monochromeWorker finished processing');
+                    }
+                })
+                monochromeWorker.on('error', (error) => {
+                    reject(new Error(error.message));
+                })
+                monochromeWorker.on('exit', (code) => {
+                    if (code !== 0) {
+                        reject(new Error('Exited with status code ' + code));
+                    }
+                })
+            } catch (error) {
+                reject(error)
+            }
+        } else {
+            reject(new Error('not on main thread'));
+        }
+    });
+}
+function uploadPathResolver(filename_) {
+    return path.resolve(__dirname, '../uploads', filename);
+}
+module.exports = imageProcessor;
