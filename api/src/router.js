6b195e0050167e0cd9b0f55f55c674f1bffd7bf7
Marc Lefkon <marc@leftek.com>
2023-03-27 13:12:20 +0800

    a

--- api/src/router.js
+++ api/src/router.js
@@ -3,37 +3,34 @@ const multer = require('multer');
 const path = require('path');
 const imageProcessor = require('./imageProcessor')
 
-function fileFilter(request, file, callback) {
-    if (file.mimetype != 'image/png') {
-        request.fileValidationError = 'Wrong file type';
-        callback(null, false, new Error('Wrong file type') );
-    } else {
-        callback(null, true);
-    }
-}
+const router = Router();
 function filename(request, file, callback) {
     callback(null, file.originalname);
 }
 
 const storage = multer.diskStorage({ destination: 'api/uploads/', filename: filename});
-const upload = multer( {fileFilter: fileFilter, storage: storage })
-const router = Router();
+function fileFilter(request, file, callback) {
+    if (file.mimetype != 'image/png') {
+        request.fileValidationError = 'Wrong file type';
+        callback(null, false, new Error('Wrong file type'));
+    } else {
+        callback(null, true);
+    }
+}
+const upload = multer({ fileFilter: fileFilter, storage: storage })
+router.post('/upload', upload.single('photo'), async (req, res) => {
+    if (req.fileValidationError) return res.status(400).json({ error: req.fileValidationError });
+    try {
+        await imageProcessor(request.file.filename);
+    } catch (error) {
+
+    }
+    return res.status(201).json({ success: true });
+})
+
 const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
 router.get('/photo-viewer', (req, res) => {
     res.sendFile(photoPath);
-})
-router.post('/upload', upload.single('photo'), async (req, res) => {
-    if (req.fileValidationError) {
-        return res.status(400)
-                  .json( {error: req.fileValidationError} );
-    }
-    try {
-        await imageProcessor( request.file.filename );
-    } catch (error) {
-        
-    }
-    return res.status(201)
-                .json( {success: true} );
 })
 
 
